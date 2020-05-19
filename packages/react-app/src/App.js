a2f2d6f7ee5ccc58eb5f1272238b0cb70dbabdfb
Petr Porobov <porobov.p@gmail.com>
2020-05-19 17:59:19 +0400

    Removed layout, added groups lists

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 
 import { Layout } from 'antd';
 
-import { Row, Col } from 'antd';
+import { Row, Col, List, Avatar } from 'antd';
 
 import { useExchangePrice, useGasPrice } from "./hooks"
 import { Logo, Account, Provider, Faucet, Ramp } from "./components"
@@ -23,49 +23,104 @@ function App() {
 
   const [address, setAddress] = useState();
   const [injectedProvider, setInjectedProvider] = useState();
+  const [activeGroupID, setactiveGroupID] = useState(0);
   const price = useExchangePrice(mainnetProvider)
   const gasPrice = useGasPrice("fast")
 
+  const [pendingGroups, setPendingGroups] = useState([]);
+
+  // let pendingGroups = []
+  let groupSuggestions = [];
+  let joinedGroups = [];
+
+
+  // test-data
+  const tempActiveGroupID1 = 12345;
+  const tempActiveGroupID2 = 435465;
+  const tempActiveGroupID3 = 234634;
+  const loadedGroups = {
+    [tempActiveGroupID1]: 
+      { 
+        "groupID": tempActiveGroupID1,
+        "title": 'Group 1',
+      },
+    [tempActiveGroupID2]:
+      {
+        "groupID": tempActiveGroupID2,
+        "title": 'Group 2',
+      },
+    [tempActiveGroupID3]:
+      {
+        "groupID": tempActiveGroupID3,
+        "title": 'Group 3',
+      },
+  }
+  for(let key in loadedGroups){
+    groupSuggestions.push(key);
+  } 
+  //end-test-data
+
+  
+  
+
+  function joinGroup(groupID) {
+    setPendingGroups(pendingGroups => [...pendingGroups, groupID]);
+    console.log(pendingGroups);
+  }
+
   return (
     <div className="App">
-      <Layout>
-      <Header>
-        <Logo />
-        <div style={{position:'fixed',textAlign:'right',right:0,top:0,padding:10}}>
-          <Account
-            address={address}
-            setAddress={setAddress}
-            localProvider={localProvider}
-            injectedProvider={injectedProvider}
-            setInjectedProvider={setInjectedProvider}
-            mainnetProvider={mainnetProvider}
-            price={price}
-          />
-        </div>
-      </Header>
-
-      <Layout>
-
-        <Sider>
-          <Groups />
-        </Sider>
-
-        <Content>
-          <div style={{padding:40,textAlign: "left"}}>
-            <Welcome
-              address={address}
-              injectedProvider={injectedProvider}
-              localProvider={localProvider}
-              price={price}
-              gasPrice={gasPrice}
-            />
-
-          </div>
-      </Content>
+      
+      <Logo />
+      {/* <div style={{position:'fixed',textAlign:'right',right:0,top:0,padding:10}}>
+        <Account
+          address={address}
+          setAddress={setAddress}
+          localProvider={localProvider}
+          injectedProvider={injectedProvider}
+          setInjectedProvider={setInjectedProvider}
+          mainnetProvider={mainnetProvider}
+          price={price}
+        />
+      </div> */}
 
 
-      <Footer>
-      <div style={{position:'fixed',textAlign:'right',right:0,bottom:20,padding:10}}>
+      Suggestions:
+      <Groups
+        loadedGroups={loadedGroups}
+        groupsList={groupSuggestions}
+        setactiveGroupID={setactiveGroupID}
+      />
+
+
+      Pending:
+      <Groups
+        loadedGroups={loadedGroups}
+        groupsList={pendingGroups}
+        setactiveGroupID={setactiveGroupID}
+      />
+
+      <div>
+        Active group: {activeGroupID}
+        Pending: {pendingGroups}
+        <a onClick={() => joinGroup(activeGroupID)}>Join</a>
+      </div>
+
+      
+      {/* <div style={{padding:40,textAlign: "left"}}>
+        <Welcome
+          address={address}
+          injectedProvider={injectedProvider}
+          localProvider={localProvider}
+          price={price}
+          gasPrice={gasPrice}
+        />
+      </div> */}
+    
+
+
+        
+      {/* <div style={{position:'fixed',textAlign:'right',right:0,bottom:20,padding:10}}>
         <Row align="middle" gutter={4}>
           <Col span={10}>
             <Provider name={"mainnet"} provider={mainnetProvider} />
@@ -94,19 +149,8 @@ function App() {
             />
           </Col>
         </Row>
-      </div>
-      </Footer>
-
-      </Layout>
-    </Layout>
-
-      
-      
-      
-
-      
-      
-
+      </div> */}
+     
     </div>
   );
 }
--- packages/react-app/src/components/Groups.js
+++ packages/react-app/src/components/Groups.js
@@ -2,36 +2,43 @@ import React from 'react';
 import { List, Avatar } from 'antd';
 
 export default function Groups(props) {
-  const data = [
-    {
-      title: 'Ant Design Title 1',
-    },
-    {
-      title: 'Ant Design Title 2',
-    },
-    {
-      title: 'Ant Design Title 3',
-    },
-    {
-      title: 'Ant Design Title 4',
-    },
-  ];
 
-  return (
-    <div>
-      <List
-        itemLayout="horizontal"
-        dataSource={data}
-        renderItem={item => (
-          <List.Item>
-            <List.Item.Meta
-              avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-              title={<a href="https://ant.design">{item.title}</a>}
-              description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-            />
-          </List.Item>
-        )}
-      />
-    </div>
-  );
+  let loadedGroups = props.loadedGroups;
+  let groupsList = props.groupsList;
+  //const hasItems = groupsList.length > 0;
+
+  let selectedGroups = [];
+  
+  for(let id in groupsList){
+    if (typeof loadedGroups[groupsList[id]] !== 'undefined' 
+        && 
+        typeof loadedGroups[groupsList[id]].title !== 'undefined') 
+        {           
+        selectedGroups.push(loadedGroups[groupsList[id]]);
+        }
+  }
+  
+  if (selectedGroups) {
+    return (
+      <div>
+          <List
+            itemLayout="horizontal"
+            dataSource={selectedGroups}
+            renderItem={item => (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                  title={<a onClick={() => props.setactiveGroupID(item.groupID)}>{item.title} </a>}
+                  description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                />
+              </List.Item>
+            )}
+          />
+        </div>
+    );
+    
+  } else {
+    return("");
+  }
+  
 }
